98fb962fa68dc66c6c00021d70f04ea8291d7f7f
Jarvis1401 <shubhamchavan6079@gmail.com>
2024-12-30 16:56:11 +0530

    This is DB File

---
+++ Backend/tms(Database).sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 30, 2024 at 12:24 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_info`
+--
+
+CREATE TABLE `task_info` (
+  `task_id` int(50) NOT NULL,
+  `t_title` varchar(120) NOT NULL,
+  `t_description` text DEFAULT NULL,
+  `t_start_time` varchar(100) DEFAULT NULL,
+  `t_end_time` varchar(100) DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT 0 COMMENT '0 = to-do, 1 = In progress, 2 = complete'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `task_info`
+--
+
+INSERT INTO `task_info` (`task_id`, `t_title`, `t_description`, `t_start_time`, `t_end_time`, `status`) VALUES
+(1, 'Demo Task', 'This is a demo task', '1/12/2024', '30/12/2024', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `password`) VALUES
+(1, 'New user', 'one@gmail.com', 'One@123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `task_info`
+--
+ALTER TABLE `task_info`
+  ADD PRIMARY KEY (`task_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `task_info`
+--
+ALTER TABLE `task_info`
+  MODIFY `task_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
